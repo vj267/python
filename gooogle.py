5949df154d1d9889936a16a6b2675907a3ebc9c9
vj267 <vinay.joshi59@gmail.com>
2019-01-19 12:26:32 +0530

    web scraping Beautiful soup

--- gooogle.py
+++ gooogle.py
@@ -1,10 +1,20 @@
 #!usr/bin/python
 
+import time
+import requests
+from bs4 import BeautifulSoup
 from googlesearch import search
 #now put a keyword
-webdata=search('hello',num=3,stop=2,pause=1)
+whattosearch=raw_input("What do you want to search")
+webdata=search('hello',num=3,tld="co.in")
 #generator type iterable
-print type(webdata)
-for i in webdata:
-	print i
+#print type(webdata)
+for url in webdata:
+	response = requests.get(url)
+	response_content= BeautifulSoup(response.content,"html.parser")
+	print url
+	complete_page=""
+	for data in response_content.find_all('p') :
+                complete_page=complete_page+ data.text
+	print complete_page
 
